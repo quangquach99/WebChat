ea059b2c38cbf51f352865cb2af0e8b42471bc3f
Dinh Quang <52022091+quangquach99@users.noreply.github.com>
2020-12-21 16:20:30 +0700

    Search User And Update Create Conversation

---
+++ Models/WebChatViewModel/SearchUsersViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebChat.Models.WebChatViewModel
+{
+    public class SearchUsersViewModel
+    {
+        public int UserID { get; set; }
+        public string UserFullname { get; set; }
+        public string UserAvatar { get; set; }
+    }
+}
